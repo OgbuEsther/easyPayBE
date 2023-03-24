485a29033b6e57e1d656881fb06dcb4770bd9d57
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-24 14:53:40 +0100

    created dist folder

--- dist/router/staff/staffAuthRoutes.js
+++ dist/router/staff/staffAuthRoutes.js
@@ -5,6 +5,6 @@ const staffAuthController_1 = require("../../controller/staff/staffAuthControlle
 const staffAuthRoutes = (0, express_1.Router)();
 staffAuthRoutes.post("/stafflogin", staffAuthController_1.staffSignin);
 staffAuthRoutes.post("/staffregister", staffAuthController_1.staffSignup);
-staffAuthRoutes.get("/ ", staffAuthController_1.getAllStaff);
+staffAuthRoutes.get("/allstaff", staffAuthController_1.getAllStaff);
 staffAuthRoutes.get("/staff/:staffId", staffAuthController_1.getOneStaff);
 exports.default = staffAuthRoutes;
