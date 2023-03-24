13e738719017d3dc42a5f9c933e4abe181ed5b46
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-24 23:55:21 +0100

    done

--- config/db.ts
+++ config/db.ts
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const DB_URI = "mongodb://0.0.0.0:27017/Easepay1";
 const LIVE_URI =
-  "mongodb+srv://Esther:Esther2004@cluster0.byfqhoj.mongodb.net/EasePay?retryWrites=true&w=majority";
+  "mongodb+srv://Esther:Esther2004@cluster0.byfqhoj.mongodb.net/EasePayApi?retryWrites=true&w=majority";
 
 const dbConfig = async () => {
   try {
-    const connect = await mongoose.connect(DB_URI);
+    const connect = await mongoose.connect(LIVE_URI);
     console.log(`database is connected to ${connect.connection.host}`);
   } catch (error) {
     console.log(`unable to connect to database ${error}`);
