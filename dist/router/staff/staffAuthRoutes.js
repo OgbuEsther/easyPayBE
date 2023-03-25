d062678c1aaadb5fd92c4e41afd373f064f0d9bb
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-25 03:17:26 +0100

    done

---
+++ dist/router/staff/staffAuthRoutes.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const staffAuthController_1 = require("../../controller/staff/staffAuthController");
+const staffAuthRoutes = (0, express_1.Router)();
+staffAuthRoutes.post("/stafflogin", staffAuthController_1.staffSignin);
+staffAuthRoutes.post("/staffregister/:id", staffAuthController_1.staffSignup);
+staffAuthRoutes.get("/allstaff", staffAuthController_1.getAllStaff);
+staffAuthRoutes.get("/staff/:staffId", staffAuthController_1.getOneStaff);
+exports.default = staffAuthRoutes;
