41ff8e0866a15d498b39c581d738b76f19ec43b9
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-04-06 18:38:31 +0100

    DEPLOYING

--- config/db.ts
+++ config/db.ts
@@ -6,7 +6,7 @@ const LIVE_URI =
 
 const dbConfig = async () => {
   try {
-    const connect = await mongoose.connect(DB_URI);
+    const connect = await mongoose.connect(LIVE_URI);
     console.log(`database is connected to ${connect.connection.host}`);
   } catch (error) {
     console.log(`unable to connect to database ${error}`);
